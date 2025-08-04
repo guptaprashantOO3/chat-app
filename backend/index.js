59ce7e7e0d1fb6174434c987b8e8e6abceb0ffb2
guptaprashantOO3 <ramjanamprashantkumar280@gmail.com>
2025-08-04 10:24:32 +0530

    abc

--- backend/index.js
+++ backend/index.js
@@ -46,11 +46,11 @@ mongoose
 app.use("/api/user", userRoute);
 app.use("/api/message", messageRoute);
 
-app.use(express.static(path.join(__dirname,'/frontend/dist')))
+app.use(express.static(path.join(__dirname, '..', 'frontend', 'dist')));
 
-app.get('*', (req,res) => {
-    res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'))
-}) 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'frontend', 'dist', 'index.html'));
+});
 
 // Start server
 server.listen(PORT, () => {
